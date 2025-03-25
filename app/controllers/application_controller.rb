227f16db1a341708aa36a57bf376c0020f594b9a
Guney Bilen <guneybilen@yahoo.com>
2025-03-25 17:26:15 -0400

    seed data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Done once only when only files are locally stored on the computer's disk.
+  # Skip if files are uploaded to a cloud storage provider.
+  include ActiveStorage::SetCurrent
 end
