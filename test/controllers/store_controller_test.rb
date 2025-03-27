3715d98d5f61e5a534200b7edac675a70c10851c
Guney Bilen <guneybilen@yahoo.com>
2025-03-27 06:50:26 -0400

    store_controller_test comments

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
+# FYI: The following does not visit Website root path on the browser.
+# It is about fixture data that is encapsulated with the application layout.
 class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
@@ -9,8 +11,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_select "main ul li", 3
     assert_select "nav ul li", 6
     assert_select "main ul", 1
+    #FYI: The 4th img is the logo on top of the page that comes from the layout.
     assert_select "img", 4
     assert_select "h2", "The Pragmatic Programmer"
     assert_select "div", /\$[,\d]+\.\d\d/
+    #puts @response.body
   end
 end
