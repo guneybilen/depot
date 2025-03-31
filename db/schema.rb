b6041bbcbf018400eaf5f018a701533f595a0c8d
Guney Bilen <guneybilen@yahoo.com>
2025-03-30 20:49:30 -0400

    main page flash bug treatment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_30_003541) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_31_003305) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,12 +46,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_30_003541) do
 
   create_table "line_items", force: :cascade do |t|
     t.integer "product_id", null: false
-    t.integer "cart_id", null: false
+    t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
-    t.integer "order_id"
     t.integer "catd_id"
+    t.integer "order_id"
+    t.decimal "price"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
